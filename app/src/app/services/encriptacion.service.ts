59a7c04a58acbd351fcfc01fa7e2e4762977bb79
Eduardo Fuentealba <eduardo.fuentealbau@gmail.com>
2024-12-20 21:27:25 -0300

    Guardar Cambios

--- app/src/app/services/encriptacion.service.ts
+++ app/src/app/services/encriptacion.service.ts
@@ -36,9 +36,15 @@ export class EncriptacionService {
 
         }
         else if(/[0-9]/.test(char)){
-          const index = this.numeros.indexOf(char);
-          const nuevaPosicion = (index + 3) % this.numeros.length;
-          return index;
+          const indexNumero = this.numeros.indexOf(char);
+
+          let nivel1PosicionNumero = 10 - indexNumero;
+          const nivel1Numero = this.numeros[nivel1PosicionNumero-1];
+
+          // Nivel 2: continuar con el desplazamiento hacia la derecha
+          const nuevaPosicionNumero = (nivel1PosicionNumero-1 + 3) % 10;
+          return this.numeros[nuevaPosicionNumero];
+          //return nuevaPosicion;
         }
         return char;
 
@@ -64,18 +70,23 @@ export class EncriptacionService {
 
           // Segundo nivel: invertir el cambio de extremo a extremo
           let nivel2Posicion = 27 - nivel1Posicion;
-          if (nivel1Letra === 'n') nivel2Posicion = 14; // Ajuste para la "ñ"
-          const nivel2Letra = this.abecedario[nivel2Posicion - 1];
+          if (nivel1Letra === 'n') nivel2Posicion = 13;
+          const nivel2Letra = this.abecedario[nivel2Posicion];
 
           return esMayuscula ? nivel2Letra.toUpperCase() : nivel2Letra;
         }
 
         // Determinar si es un número
         if (/[0-9]/.test(char)) {
-          const index = this.numeros.indexOf(char);
-          const nuevaPosicion =
-            (index - 3 + this.numeros.length) % this.numeros.length;
-          return this.numeros[nuevaPosicion];
+          // Nivel 2: revertir el desplazamiento hacia la derecha (desplazar 3 posiciones hacia la izquierda)
+          const indexNumero = this.numeros.indexOf(char);
+
+          const nivel2PosicionNumero = (indexNumero - 3 + 10) % 10;
+
+          // Nivel 1: revertir el cálculo del extremo
+          const nivel1PosicionNumero = (10 - nivel2PosicionNumero);
+
+          return this.numeros[nivel1PosicionNumero -1];
         }
 
         // Si no es letra ni número, se deja igual
@@ -83,8 +94,4 @@ export class EncriptacionService {
       })
       .join('');
   }
-
-
-
-
 }
